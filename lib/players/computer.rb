8099c24fe14fb7b4171be9e2b980f26f9dbca71d
Matthew Hunger <matt.hunger@gmail.com>
2017-12-07 15:34:09 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,6 +10,13 @@ module Players
       end
       #valid_moves
       #binding.pry
+      best_moves = []
+      WIN_COMBINATIONS.each do |combo|
+        if @board.cells[combo[0]] == @board.cells[combo[1]]  &&  @board.cells[combo[2]] == " "
+          if @board.cells[combo[0]] != " "
+            return @board.cells[combo[0]]
+          end
+        end
       return valid_moves[0]
 
     end
